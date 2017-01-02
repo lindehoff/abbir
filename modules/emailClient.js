741d04c32216b7176c291b318e23528dd3c1477d
lindehoff <jacob@cobmedia.se>
2017-01-02 13:54:42 +0100

    Adds sample config

--- modules/emailClient.js
+++ modules/emailClient.js
@@ -6,3 +6,26 @@
  */
 
 'use strict';
+const MailListener = require('mail-listener2');
+
+function EmailClient(config, logger) {
+  let mailListener = new MailListener(config);
+  mailListener.on('server:connected', function(){
+    logger.info('[%s] imapConnected', customerConfig.customerInfo.name);
+  });
+
+  mailListener.on('server:disconnected', function(){
+    logger.warn('[%s] imapDisconnected', customerConfig.customerInfo.name);
+    this.start();
+  });
+
+  mailListener.on('error', function(err){
+    logger.error('[%s] imap error: ', customerConfig.customerInfo.name, err);
+    this.start();
+  });
+}
+
+// Exports
+module.exports = {
+  EmailClient
+};
