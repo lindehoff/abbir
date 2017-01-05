0915334fa0b60c57ee86627acad7357bca502a45
lindehoff <jacob@cobmedia.se>
2017-01-05 16:49:12 +0100

    Changed incomming folder structure and adds info.json for later processing

--- modules/EmailClient.js
+++ modules/EmailClient.js
@@ -10,6 +10,8 @@ const MailListener = require('mail-listener2');
 const fs = require('fs');
 const mkdirp = require('mkdirp');
 const async = require('async');
+const util = require('util');
+const _ = require('lodash');
 
 function EmailClient(config, logger) {
   const mailListener = new MailListener(config.mailListener);
@@ -31,14 +33,32 @@ function EmailClient(config, logger) {
       config.abbir.screenName,
       mail.messageId,
       attributes);
-    if (config.abbir.acceptedUsers.some(x => x.emailAddress.includes(mail.from[0].address))) {
+    let user = config.abbir.acceptedUsers.find(x => x.emailAddress.includes(mail.from[0].address));
+    if (user) {
       logger.info('[%s] Email address in acceptedEmailAddress: %s',
         config.abbir.screenName,
         mail.from[0].address);
       if (mail.hasOwnProperty('attachments')) {
-        let path = config.abbir.incommingPath + '/' + mail.messageId;
+        let path = config.abbir.incommingPath + '/images/' + user.name;
+        let title = util.format('%s from %s', mail.date.toLocaleDateString('sv-SE'), user.name);
         if (mail.subject) {
-          path += '/' + mail.subject;
+          title = mail.subject;
+        } else {
+          let lines = mail.text.split('\n');
+          if(lines.length > 0 && /\S/.test(lines[0])) {
+            title = lines[0].trim();
+          }
+        }
+        path += '/' + title;
+        let info = {
+          date: mail.date,
+          from: mail.from[0].address,
+          user: user.name,
+          subject: mail.subject,
+          messageId: mail.messageId,
+          text: mail.text,
+          title: title,
+          attachments: []
         }
         let imageCount = 0;
         mkdirp(path, function(err) {
@@ -49,20 +69,28 @@ function EmailClient(config, logger) {
               err);
           } else {
             async.each(mail.attachments, function(attachment, callback) {
+              let infoAttachment = _.pick(attachment, ['checksum', 'contentId', 'contentType', 'generatedFileName']);
+              info.attachments.push(infoAttachment);
               if (attachment.contentType === 'image/jpeg') {
+                ++imageCount;
                 logger.info('[%s] attachment found in %s: ',
                   config.abbir.screenName,
                   mail.messageId,
                   attachment.generatedFileName);
-                fs.writeFile(path + '/' + attachment.generatedFileName, attachment.content, function(err) {
+                let filePath =  util.format('%s/%s.jpg',
+                  path,
+                  attachment.generatedFileName);
+                fs.writeFile(filePath, attachment.content, function(err) {
                   if (err) {
                     logger.warn('[%s] Unable to save image %s, Error: ',
                       config.abbir.screenName,
                       attachment.generatedFileName,
                       err);
-                  } else {
-                    ++imageCount;
+                    infoAttachment.status = err;
+                    --imageCount;
                   }
+
+                  infoAttachment.status = 'Saved: ' + filePath;
                   callback();
 
                 });
@@ -73,6 +101,7 @@ function EmailClient(config, logger) {
                   mail.messageId,
                   attachment.contentType);
                 callback();
+                infoAttachment.status = 'Not saved';
               }
             }, function(err) {
               if (err) {
@@ -86,6 +115,11 @@ function EmailClient(config, logger) {
                   imageCount,
                   mail.attachments.length,
                   mail.messageId);
+              }
+              if(imageCount > 0) {
+                fs.writeFile(path + '/info.json', JSON.stringify(info, null, 2) , 'utf-8');
+              } else {
+                //todo: remove path
               }
             });
           }
