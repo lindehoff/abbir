5809c6031be527ad17dc0f34e3ffdb0abe66a4cd
lindehoff <jacob@cobmedia.se>
2017-02-23 11:26:24 +0000

    Adds close method, removes listeners

--- modules/IrRemote.js
+++ modules/IrRemote.js
@@ -13,20 +13,30 @@ const lircNode = require('lirc_node');
 function IrRemote(config, logger) {
   EventEmitter.call(this);
   const self = this;
+  let listeners = [];
   logger.info('[IrRemote] Initializing LIRC');
   lircNode.init(function() {
     Object.keys(lircNode.remotes).forEach(function(remote) {
       logger.info('[IrRemote] Adding listeners for remote %s', remote);
       lircNode.remotes[remote].forEach(function(button) {
         logger.info('[IrRemote %s] Adding listener for button %s', remote, button);
-        lircNode.addListener(button, remote, function(data) {
+        listeners.push(lircNode.addListener(button, remote, function(data) {
           if (data.repeat === '00') {
-              self.emit('buttonPress', button);
-            }
-          }, 500);
+            self.emit('buttonPress', button);
+          }
+        }, 500));
       });
     });
   });
+  this.stop = function() {
+    Object.keys(lircNode.remotes).forEach(function(remote) {
+      logger.info('[IrRemote] Removing listeners for remote');
+      listeners.forEach(function(listener) {
+        logger.info('[IrRemote] Removing listener %s', listener);
+        lircNode.removeListener(listener);
+      });
+    });
+  };
 }
 
 util.inherits(IrRemote, EventEmitter);
