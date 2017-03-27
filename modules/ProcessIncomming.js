f27d678d7a3f0dd22a8b9eafd99a2b3c9c8f609b
lindehoff <jacob@cobmedia.se>
2017-03-27 14:21:50 +0200

    Working on promisifying process incomming

--- modules/ProcessIncomming.js
+++ modules/ProcessIncomming.js
@@ -48,118 +48,138 @@ function ProcessIncomming() {
     };
     readdir(processedData.path)
     .then(files => {
-      let albumInfoPath = files.find(fileName => fileName === 'info.json');
-      if (albumInfoPath) {
-        readJSON(path + albumInfoPath)
-        .then(albumInfo => {
-          processedData.albumInfo = albumInfo;
-          return User.findOne({'email': albumInfo.from}).exec()
-        })
-        .then(owner =>  {
-          processedData.owner = owner;
-          processedData.album = new Album({name: processedData.albumInfo.title, owner: owner});
-          return processedData.album.save();
-        })
-        .then(album => {
-          processedData.album = album;
-          return ResourceType.findOne({'name': 'image'}).exec()
-        })
-        .then(imageResourceType =>  {
-          let resourceDetail = {
-            name: null,
-            created: null,
-            fileName: null,
-            owner: processedData.owner,
-            resourceType: imageResourceType,
-            album: processedData.album
-          };
-          async.each(files, function(file, callback) {
-            let filePath = path + file;
-            let fileData;
-            let resource;
-            if (pathUtil.extname(file).toLowerCase() === '.jpg') {
-              readFile(filePath)
-              .then(data => {
-                fileData = data;
-                console.log('Data: ', data.length);
-                return getImageDate(fileData);
-              })
-              .then(date => {
-                resourceDetail.created = date;
-                resourceDetail.fileName = util.format('%s [%s] %s.jpg',
-                  date.format('YYYY-MM-DDThh.mm.ss'),
-                  processedData.album.owner.userName,
-                  processedData.album.name
-                );
-                resourceDetail.name = resourceDetail.fileName;
-                resource = new Resource(resourceDetail);
-                return backup(fileData, resource.localPath);
-              })
-              .then(onlineUrl => {
-                resource.onlineUrl = onlineUrl;
-                return mkdirp(resource.localPath);
-              })
-              .then(() => {
-                gm(fileData)
-                .resize(config.abbir.imageSize, config.abbir.imageSize)
-                .write(resource.localPath, function(err) {
-                  if (err) {
-                    logger.warn('[ProcessIncomming] Unable to save/resize image %s from email [%s], Error: ',
-                      resource.fileName,
-                      processedData.albumInfo.messageId,
-                      err);
-                  } else {
-                    logger.info('[ProcessIncomming] Image %s saved and resized from email [%s]',
-                      resource.fileName,
-                      processedData.albumInfo.messageId);
-                    exec('exiftran -ai \'' + resource.localPath + '\'', function() {
-                      logger.info('[ProcessIncomming] Updating timestamp in image %s to %s',
-                        resource.localPath,
-                        date);
-                      fs.utimes(resource.localPath, new Date(date) / 1000, new Date(processedData.albumInfo.date) / 1000, function(err) {
-                        if (err) {
-                          logger.warn('[ProcessIncomming] Unable to update timestamp in image %s to %s, Error: ',
-                            resource.localPath,
-                            date,
-                            err);
-                        } else {
-                          logger.info('[ProcessIncomming] Image %s date changed]',
-                            resource.localPath);
-                        }
-                        fs.unlinkSync(filePath);
-                        resource.save()
-                        .then(imageResource => {
-                          processedData.newResources.push(imageResource);
-                          callback();
-                        })
-                        .catch(err => {
-                          logger.error('[ProcessIncomming] Unable to save resource %s to database, Error: ',
-                            resource.localPath,
-                            err);
-                          callback(err);
+      return new Promise(function(fulfill, reject) {
+        let albumInfoPath = files.find(fileName => fileName === 'info.json');
+        if (albumInfoPath) {
+          readJSON(path + albumInfoPath)
+          .then(albumInfo => {
+            processedData.albumInfo = albumInfo;
+            return User.findOne({'email': albumInfo.from}).exec()
+          })
+          .then(owner =>  {
+            if (owner) {
+              processedData.owner = owner;
+              processedData.album = new Album({name: processedData.albumInfo.title, owner: owner});
+              return processedData.album.save();
+            } else {
+              throw new Error('Not an valid email address: ' + albumInfo.from);
+            }
+          })
+          .then(album => {
+            processedData.album = album;
+            return ResourceType.findOne({'name': 'image'}).exec()
+          })
+          .then(imageResourceType =>  {
+            let resourceDetail = {
+              name: null,
+              created: null,
+              fileName: null,
+              owner: processedData.owner,
+              resourceType: imageResourceType,
+              album: processedData.album
+            };
+            async.each(files, function(file, callback) {
+              let filePath = path + file;
+              let fileData;
+              let resource;
+              if (pathUtil.extname(file).toLowerCase() === '.jpg') {
+                readFile(filePath)
+                .then(data => {
+                  fileData = data;
+                  return getImageDate(fileData);
+                })
+                .then(date => {
+                  resourceDetail.created = date;
+                  resourceDetail.fileName = util.format('%s [%s] %s.jpg',
+                    date.format('YYYY-MM-DDThh.mm.ss'),
+                    processedData.album.owner.userName,
+                    processedData.album.name
+                  );
+                  resourceDetail.name = resourceDetail.fileName;
+                  resource = new Resource(resourceDetail);
+                  return backup(fileData, resource.localPath);
+                })
+                .then(uploadInfo => {
+                  resource.onlineUrl = uploadInfo.Location;
+                  return mkdirp(pathUtil.dirname(resource.localPath));
+                })
+                .then(() => {
+                  gm(fileData)
+                  .resize(config.abbir.imageSize, config.abbir.imageSize)
+                  .write(resource.localPath, function(err) {
+                    if (err) {
+                      logger.warn('[ProcessIncomming] Unable to save/resize image %s from email [%s], Error: ',
+                        resource.fileName,
+                        processedData.albumInfo.messageId,
+                        err);
+                    } else {
+                      logger.info('[ProcessIncomming] Image %s saved and resized from email [%s]',
+                        resource.fileName,
+                        processedData.albumInfo.messageId);
+                      exec('exiftran -ai \'' + resource.localPath + '\'', function() {
+                        logger.info('[ProcessIncomming] Updating timestamp in image %s to %s',
+                          resource.localPath,
+                          resource.created);
+                        fs.utimes(resource.localPath, new Date(resource.created) / 1000, new Date(resource.added) / 1000, function(err) {
+                          if (err) {
+                            logger.warn('[ProcessIncomming] Unable to update timestamp in image %s to %s, Error: ',
+                              resource.localPath,
+                              resource.created,
+                              err);
+                          } else {
+                            logger.info('[ProcessIncomming] Image %s date changed]',
+                              resource.localPath);
+                          }
+                          fs.unlinkSync(filePath);
+                          resource.save()
+                          .then(imageResource => {
+                            logger.info('[ProcessIncomming] Resource saved to DB: %s',
+                              resource);
+                            processedData.newResources.push(imageResource);
+                            callback();
+                          })
+                          .catch(err => {
+                            logger.error('[ProcessIncomming] Unable to save resource %s to database, Error: ',
+                              resource.localPath,
+                              err);
+                            callback(err);
+                          });
                         });
                       });
-                    });
-                  }
+                    }
+                  });
+                })
+                .catch(err => {
+                  logger.error('[ProcessIncomming] Unable to process file %s, error: ',
+                    filePath,
+                    err);
                 });
-              })
-              .catch(err => {
-                logger.error('[ProcessIncomming] Unable to process file %s, error: ',
-                  filePath,
-                  err);
-              });
-            } else {
-              logger.info('[ProcessIncomming] Not an jpg image',
-                filePath);
-            }
+              } else {
+                logger.info('[ProcessIncomming] Not an jpg image',
+                  filePath);
+              }
+            }, err => {
+              if (err) {
+                reject(util.format('[ProcessIncomming] Unable to to process %s, error: %s',
+                  filePath, err)
+                );
+              } else {
+                fulfill();
+              }
+            });
+          })
+          .catch(function(err) {
+            logger.error('Error: %s', err);
           });
-        });
-      } else {
-        logger.warn('[ProcessIncomming] Unable to find info.json in %s',
-          filePath);
-      }
+        } else {
+          logger.warn('[ProcessIncomming] Unable to find info.json in %s',
+            filePath);
+          reject('[ProcessIncomming] Unable to find info.json');
+        }
+      });
     })
     .then(() => {
+      logger.warn('[ProcessIncomming] All done')
       self.emit('newImages', processedData.newResources);
     })
     .catch(function(err) {
@@ -172,8 +192,10 @@ function ProcessIncomming() {
       if (config.amazon.backup) {
         let hash = crypto.createHash('sha256');
         hash.update(path);
+        let key = util.format('images/%s%s', hash.digest('hex'), pathUtil.extname(path));
+        logger.debug('[ProcessIncomming] Uploading %s to AWS S3 with Key: %s', path, key);
         s3.upload({
-          Key: hash.digest('hex'),
+          Key: key,
           Body: fileBuffer,
           ContentEncoding: 'base64',
           Metadata: {
@@ -184,7 +206,6 @@ function ProcessIncomming() {
           if (err) {
             reject(err);
           } else {
-            logger.info('Data: %s', data);
             fulfill(data);
           }
         });
@@ -197,7 +218,6 @@ function ProcessIncomming() {
   let getImageDate = function(file, callback) {
     return new Promise(function(fulfill, reject) {
       try {
-        console.log('tyope: ', typeof(file))
         gm(file)
         .identify(function(err, data) {
           if (err) {
